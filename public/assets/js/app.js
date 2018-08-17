e5ad9fd8e4dec5942497287e8bffacb5c38ed725
hankzimmer7 <hankzimmer7@gmail.com>
2018-08-17 18:38:43 -0400

    adds notes modal popup

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -153,7 +153,7 @@ $(document).on("click", "#saved-articles", function () {
         link.text(data[i].title);
 
         var noteButton = $("<button>");
-        noteButton.addClass("btn btn-primary note-button");
+        noteButton.addClass("btn btn-primary notes-button");
         noteButton.attr("data-id", data[i]._id);
         noteButton.text("Notes");
 
@@ -181,12 +181,64 @@ $(document).on("click", "#saved-articles", function () {
   });
 });
 
+//When the user clicked the notes button, display the notes modal
+$(document).on("click", ".notes-button", function () {
+  console.log("Clicked the notes button");
+  const modal = document.getElementById("notes-modal");
+  $("#notes").empty();
+  modal.style.display = "block";
+
+  var thisId = $(this).attr("data-id");
+
+  console.log("thisId: " + thisId);
+    // Now make an ajax call for the Article
+    $.ajax({
+      method: "GET",
+      url: "/articles/" + thisId + "/notes"
+    })
+    // With that done, add the note information to the page
+    .then(function (data) {
+      console.log(data);
+      // The title of the article
+      $("#notes").append("<h2>" + data.title + "</h2>");
+      // An input to enter a new title
+      $("#notes").append("<input id='titleinput' name='title' >");
+      // A textarea to add a new note body
+      $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+      // A button to submit a new note, with the id of the article saved to it
+      $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+
+      // If there's a note in the article
+      if (data.note) {
+        // Place the title of the note in the title input
+        $("#titleinput").val(data.note.title);
+        // Place the body of the note in the body textarea
+        $("#bodyinput").val(data.note.body);
+      }
+    });
+});
+
+// When the user clicks on <span> (x), close the notes modal
+$(document).on("click", "#close-notes-button", function () {
+  const modal = document.getElementById("notes-modal");
+  modal.style.display = "none";
+});
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  const modal = document.getElementById("notes-modal");
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 // Whenever someone clicks a p tag
 $(document).on("click", "p", function () {
   // Empty the notes from the note section
   $("#notes").empty();
   // Save the id from the p tag
   var thisId = $(this).attr("data-id");
+
 
   // Now make an ajax call for the Article
   $.ajax({
